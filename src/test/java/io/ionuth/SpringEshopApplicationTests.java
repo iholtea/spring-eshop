a2dc9b60aa7ab0827ee9c983a918d7e92c89e6f0
iholtea <iholtea@gmail.com>
2023-11-14 13:33:41 +0200

    simple spring web project added
    contains also a HelloWorld endpoint

---
+++ src/test/java/io/ionuth/SpringEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ionuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
